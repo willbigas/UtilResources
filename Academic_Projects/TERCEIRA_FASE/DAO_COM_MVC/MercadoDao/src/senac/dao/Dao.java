b2b8546a2a858310bfdb7a04e9affc72ccbdbad0
willbigas <williambmauro@hotmail.com>
2019-03-15 17:12:18 -0300

    Updated.

--- Academic_Projects/TERCEIRA_FASE/DAO_COM_MVC/MercadoDao/src/senac/dao/Dao.java
+++ Academic_Projects/TERCEIRA_FASE/DAO_COM_MVC/MercadoDao/src/senac/dao/Dao.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package senac.dao;
 
 import java.sql.Connection;
-import java.util.List;
 import senac.factory.Conexao;
-import senac.interfaces.DaoI;
-import senac.model.Produto;
 
 /**
  *
